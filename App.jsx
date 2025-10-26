7180ff598cf7aa91d3be542edab8d28762b0fcef
suriyaprabhamathivanan <suriyaprabha9401@mountzion.ac.in>
2025-10-26 10:59:41 +0530

    Update App.jsx

--- App.jsx
+++ App.jsx
@@ -1,4 +1,3 @@
-// App.jsx
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
@@ -16,4 +15,3 @@ function App() {
 }
 
 export default App;
-
