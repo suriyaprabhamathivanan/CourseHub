691e38299a8eb06da56628fdf01c5d093463aa2e
Santhosh's Mac <santhosh@santhoshs-MacBook-Air.local>
2025-10-26 10:22:20 +0530

    Move all files to root

--- App.jsx
+++ App.jsx
@@ -1,18 +1,16 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Main from "./pages/Main";
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/main" element={<Main />} />
-      </Routes>
-    </Router>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/main" element={<Main />} />
+    </Routes>
   );
 }
 
